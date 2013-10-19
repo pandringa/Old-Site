550e56947e22171134199f167a47f3dc2767e728
Peter Andringa <peter9221@gmail.com>
2013-10-19 15:09:55 -0400

    temp dont start blog

--- start.js
+++ start.js
@@ -11,8 +11,8 @@ var port = 3000;
 httpProxy.createServer({
 	hostnameOnly: true,
 	router: {
-	'peterandrin.ga': 'localhost:3001',
-	'blog.peterandrin.ga': 'localhost:3002'
+	'peterandrin.ga': 'localhost:3001'
+	//'blog.peterandrin.ga': 'localhost:3002'
 	}
 }).listen(port);
 
@@ -21,5 +21,5 @@ httpProxy.createServer({
 util.puts('http proxy server '.blue + 'started '.green.bold + 'on port '.blue + (port+' ').yellow + 'with proxy table'.magenta.underline);
 
 
-require('./blog/index.js');
+//require('./blog/index.js');
 require('./server/app.js');
